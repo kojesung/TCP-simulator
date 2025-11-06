310a6b11c7edb38c5457c6e548418acd3805cb60
kojesung <js95112345@gmail.com>
2025-11-06 23:40:42 +0900

    feat(Packet): Packet 클래스 생성

---
+++ src/Packet.js
@@ -0,0 +1,14 @@
+class Packet {
+    constructor(id, startSeq, endSeq) {
+        this.id = id;
+        this.startSeq = startSeq;
+        this.endSeq = endSeq;
+        this.packetSize = endSeq - startSeq + 1;
+    }
+
+    getPacketInfo() {
+        return `Packet#${this.id} (seq#${this.startSeq} - ${this.endSeq}, ${this.packetSize})`;
+    }
+}
+
+export default Packet;
